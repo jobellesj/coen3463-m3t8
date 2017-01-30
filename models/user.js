2cacff4c59c9fac47d4ac961ee792f921a6b2b8d
fashioncelle <jocelle.tanquion@gmail.com>
2017-01-30 19:44:55 +0800

    Update user.js

--- models/user.js
+++ models/user.js
@@ -39,11 +39,11 @@ module.exports.getUserById = function(id, callback){
 }
 
 module.exports.comparePassword = function(candidatePassword, hash, callback){
-    bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
-        if(err) {
-            throw err;
-        }
-        callback(null, isMatch);
-    });
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    	if(err){
+    		console.log(err);
+    	}
+    	callback(null, isMatch);
+	});
 }
 
